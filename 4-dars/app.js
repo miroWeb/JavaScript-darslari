c8599326391ffe13c66c5d8227fa53b29a782df5
miroWeb <mirodiljs2004@gmail.com>
2024-02-14 18:40:54 +0500

    JS

---
+++ 4-dars/app.js
@@ -0,0 +1,34 @@
+// let son1 = 110;
+
+// console.log(son1 % 10);
+//===================
+// let a = 10;
+
+// console.log(--a);
+//=======================
+// let a = 10;
+// // a = a + 10;
+// a += 10;
+// console.log(a);
+//=======================
+//OR ||
+// let ega = false;
+// let admin = true;
+
+// console.log(ega || admin);
+//=======================
+//AND &&
+// let salom = true;
+// let hayr = false;
+// console.log(salom && hayr);
+//=======================
+// let a = 2;
+// let x = 1 + (a *= 2);
+// console.log(x);
+let a = 1,
+  b = 1;
+
+let c = ++a;
+let d = b++;
+
+console.log(d);
