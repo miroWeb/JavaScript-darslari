5cd52f339cee9c4e92e9db5062d11de0f414ebb2
miroWeb <mirodiljs2004@gmail.com>
2024-02-19 11:57:33 +0500

    if else

---
+++ 5-dars/app.js
@@ -0,0 +1,63 @@
+// let b = "10";
+// let a = 10;
+
+// console.log(b == a);
+// console.log(b === a);
+// console.log(typeof b);
+// console.log(typeof a);
+//==========================
+
+// let son1 = 10;
+// let son2 = 10;
+
+// console.log(son1 != son2);
+
+// let text2 = "A";
+// let text1 = "a";
+
+// console.log(text1 < text2);
+
+// let ism = prompt("ism kiriting");
+
+// console.log(ism);
+// ===================================
+// let temp = true;
+// let son = false;
+
+// if (son == true) {
+//   console.log("togri");
+// }
+
+// if (son == false) {
+//   console.log("error");
+// }
+
+// let son = prompt("son kirting");
+
+// if (son >= 10) {
+//   console.log("good");
+// } else {
+//   console.log("error");
+// }
+
+// let temp = "tarnado"; // issiq, sovuq, qor
+
+// if (temp === "issiq") {
+//   console.log("Havo issiq, qalin kiying");
+// } else if (temp === "sovuq") {
+//   console.log("Havo sovuq, yengil kiying");
+// } else if (temp === "qor") {
+//   console.log("Havo qor");
+// } else {
+//   console.log("No'malum ob-havo");
+// }
+
+let temp = 11;
+
+if (temp < 0) {
+  console.log("juda sovuq");
+} else if (temp < 10) {
+  console.log("sovuq");
+} else if (temp < 15) {
+  console.log("issiq");
+}
