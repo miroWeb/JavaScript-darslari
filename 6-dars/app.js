eceba839bb9184f4c2bc1d8e07b5c1c7c007d17a
miroWeb <mirodiljs2004@gmail.com>
2024-02-19 19:30:16 +0500

    if else terarey operator

---
+++ 6-dars/app.js
@@ -0,0 +1,46 @@
+// let trupka = prompt("qo'ng'iroq");
+
+// if (trupka === "ko'tardi") {
+//   console.log("do'stim \nyaxshimisan");
+// } else {
+//     console.log("do'stingiz trubkani ko'tarmadi");
+// }
+
+// let baho = prompt("baho kirting");
+
+// if (baho <= 3) {
+//   console.log("o'ta olmadingiz");
+// } else {
+//   console.log("o'tdingiz");
+// }
+
+// ? -> if,  : -> else
+
+// let son = 10;
+
+// son === 10 ? console.log("good") : console.log("error");
+
+// if (son === 10) {
+//   console.log("good");
+// } else {
+//   console.log("error");
+// }
+//====================
+
+// let mal = prompt("ma'lumot kiriting");
+
+// alert(mal);
+
+let pul = prompt("summa kiriting");
+
+if (pul >= 1000) {
+  alert("TURKIYA");
+} else if (pul >= 500) {
+  alert("TOSHKENT");
+} else if (pul >= 100) {
+  alert("BOLAJON BOG'I");
+} else if (pul >= 10) {
+  alert("UYDA O'TIR");
+} else {
+  console.log("mo'malum summa");
+}
