82b69ff352702528ba5deb48f393c7891c9f1f22
miroWeb <mirodiljs2004@gmail.com>
2024-03-01 16:04:18 +0500

    Function Basics

---
+++ 11-dars/app.js
@@ -0,0 +1,76 @@
+//Function Decloration -> function test() {}
+// Function Expression  -> const test = function() {}
+// Arrow Function -> const test = () ⇒ {}
+
+// let, var, const
+
+// function test() {
+//   console.log("wecome");
+// }
+// test();
+
+// const test = function (name, surname, age) {
+//   console.log(`name: ${name}`);
+//   console.log(`surname: ${surname}`);
+//   console.log(`age: ${age}`);
+// };
+
+// test("Farhod", "Samiyev", 19); //argument
+// test("Behruzbek", "Mo'minov", 14); //argument
+// test("Amalbek", "Ibragimov", 15); //argument
+
+// let b = 10;
+// let a = "10";
+
+// console.log(b == a);
+// console.log(b === a);
+// console.log(typeof b);
+// console.log(typeof a);
+
+// AND $$
+// let faceID = true;
+// let ID = false;
+
+// console.log(faceID && ID);
+
+// let ism = "Sardor";
+// let ism = prompt("ism kiriting....");
+
+// console.log(ism);
+// let son = false;
+
+// if (son === true) {
+//   console.log("good");
+// }
+
+// if (son === false) {
+//   console.log("error");
+// }
+
+// let son = prompt("son kiritin...");
+
+// if (son >= 10) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+// issiq, iliq, sovuq, minus,
+
+// let temp = prompt("ob havo ma'lumtini kiriting....");
+
+if (temp === "issiq") {
+  console.log("Havo issiq");
+} else if (temp === "iliq") {
+  console.log("havo iliq");
+} else if (temp === "sovuq") {
+  console.log("Havo sovuq");
+} else if (temp === "minus") {
+  console.log("Havo juda sovuuq");
+} else {
+  console.log("bunday ma'lumot mavjud emas");
+}
+
+// let nom = prompt("nimadur yoz...");
+
+// alert(nom);
+// console.log(nom);
